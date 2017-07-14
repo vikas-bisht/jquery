30cc98fd2ed9444728f1c45e71b1103d48def552
vikas-bisht <deepak.excel2011@gmail.com>
2017-07-14 14:53:02 +0530

    well formated files

--- ajax.js
+++ ajax.js
@@ -1,15 +1,15 @@
 function validateEmail(email)
+{
+	var cemail = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;		
+	if (cemail.test(email))
 	{
-		var cemail = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;		
-		if (cemail.test(email))
-		{
-			return true; 
-		}
-		else
-		{
-			return false;
-		}
+		return true; 
 	}
+	else
+	{
+		return false;
+	}
+}
 $(document).ready(function(){
 	$('#submit').click(function(e){
 		e.preventDefault();
@@ -51,29 +51,29 @@ $(document).ready(function(){
 				dataType:'html',
 				data: {"name" : name,"email" : email,"message" : message,"date" : date},
 				success:function(data){
-					alert('submitted');
-						$('#name').val("");
-						$('#email').val("");
-						$('#message').val("");
-						$('#date').val("");
+				alert('submitted');
+					$('#name').val("");
+					$('#email').val("");
+					$('#message').val("");
+					$('#date').val("");
 				}
 			});
 		}
 	});
 });
 function data_refresh()
-	{
-		$.ajax({
-			url:'fetch.php',
-			type:'POST',
-			success:function(response){
-				var data = JSON.parse(response);
-				var showdata =("<thead><tr><th>Name</th><th>Email</th><th>Message</th><th>Date</th></tr></thead>");
-				$.each(data,function(i){
-					showdata +=("<tr><td>"+data[i].name+"</td><td>"+data[i].email+"</td><td>"+data[i].message+"</td><td>"+data[i].date+"</td></tr>");
-				});
-				$('#result').html(showdata);
-			}
-		});	
-	}
-	setInterval(data_refresh,1000);
+{
+	$.ajax({
+		url:'fetch.php',
+		type:'POST',
+		success:function(response){
+			var data = JSON.parse(response);
+			var showdata =("<thead><tr><th>Name</th><th>Email</th><th>Message</th><th>Date</th></tr></thead>");
+			$.each(data,function(i){
+				showdata +=("<tr><td>"+data[i].name+"</td><td>"+data[i].email+"</td><td>"+data[i].message+"</td><td>"+data[i].date+"</td></tr>");
+			});
+			$('#result').html(showdata);
+		}
+	});	
+}
+setInterval(data_refresh,1000);
